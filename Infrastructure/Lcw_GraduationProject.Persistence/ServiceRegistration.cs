30caf513a1cdcec906efc3de5280efde22202ebd
sydenes <s.enessagban@gmail.com>
2022-05-28 00:50:55 +0300

    For Product, entity, abstraction and concrete created and added HttpGet Api

---
+++ Infrastructure/Lcw_GraduationProject.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Lcw_GraduationProject.Application.Abstractions;
+using Lcw_GraduationProject.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lcw_GraduationProject.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
