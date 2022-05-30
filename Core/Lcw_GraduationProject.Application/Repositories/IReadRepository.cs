64f3a7f500aafd227d1eaef105d9890984d3648d
sydenes <s.enessagban@gmail.com>
2022-05-31 02:04:10 +0300

    Tracking control added.

--- Core/Lcw_GraduationProject.Application/Repositories/IReadRepository.cs
+++ Core/Lcw_GraduationProject.Application/Repositories/IReadRepository.cs
@@ -10,9 +10,9 @@ namespace Lcw_GraduationProject.Application.Repositories
 {
     public interface IReadRepository<T>:IRepository<T> where T:BaseEntity
     {
-        IQueryable<T> GetAll();
-        IQueryable<T> GetWhere(Expression<Func<T,bool>> filter);
-        Task<T> GetSingleAsync(Expression<Func<T, bool>> filter);
-        Task<T> GetByIdAsync(string id);
+        IQueryable<T> GetAll(bool tracking=true);
+        IQueryable<T> GetWhere(Expression<Func<T,bool>> filter, bool tracking = true);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> filter, bool tracking = true);
+        Task<T> GetByIdAsync(string id, bool tracking = true);
     }
 }
--- Infrastructure/Lcw_GraduationProject.Persistence/Repositories/ReadRepository.cs
+++ Infrastructure/Lcw_GraduationProject.Persistence/Repositories/ReadRepository.cs
@@ -6,7 +6,7 @@ using System.Linq.Expressions;
 
 namespace Lcw_GraduationProject.Persistence.Repositories
 {
-    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity //Marker Pattern kullanılarak gönderilen class'ın spesifik olarak BaseEntity'den türeyecek bir class olduğunu belirterek Lambda Expression ile 'id' bilgisine generic olarak ulaştık.
     {
         private readonly LcwAPIDbContext _context;
 
@@ -17,16 +17,37 @@ namespace Lcw_GraduationProject.Persistence.Repositories
 
         public DbSet<T> Table => _context.Set<T>(); //Generic gelen Entity class'ı Set<T> ile DbSet'e eşlendi.
 
-        public IQueryable<T> GetAll()
-            => Table;
+        public IQueryable<T> GetAll(bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking) //Tracking: EfCore'un database'den gelen verileri takip eden mekanizması. Optimizasyon için gerek olmayan kısımlar tracking'i kapatabileceğimiz bir kontrol yapısı ekledik. Tracking kapatılırsa SaveChanges metotu db'yi o nesne için etkilemez.
+                query = query.AsNoTracking();
+            return query;
+        }
 
-        public async Task<T> GetByIdAsync(string id)
-            =>await Table.FirstOrDefaultAsync(data=>data.Id==Guid.Parse(id));
+        public async Task<T> GetByIdAsync(string id, bool tracking = true)
+        //=> await Table.FindAsync(Guid.Parse(id)); //marker yerine kullanılabilir ancak Queryable'da Find metotu bulunmadığı için marker'la devam ettik.
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+        }
 
-        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> filter)
-            =>await Table.FirstOrDefaultAsync(filter);
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> filter, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await query.FirstOrDefaultAsync(filter);
+        }
 
-        public IQueryable<T> GetWhere(Expression<Func<T, bool>> filter)
-            => Table.Where(filter);
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> filter, bool tracking = true)
+        {
+            var query = Table.Where(filter);
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
     }
 }
